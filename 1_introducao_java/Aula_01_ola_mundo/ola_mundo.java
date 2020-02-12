6db529fcc53768db1bf297313e72467f55856523
Daniel Wzoreck <wzoreck2000@gmail.com>
2020-02-11 23:54:09 -0300

    aula 02 java - inicio classe

--- 1_introducao_java/Aula_01_ola_mundo/ola_mundo.java
+++ 1_introducao_java/Aula_01_ola_mundo/ola_mundo.java
@@ -2,7 +2,6 @@
 public class ola_mundo { // utilizar o nome desta classe igual ao nome do arquivo
     public static void main(String[] args) {
         System.out.println("Hello world :)");
-    }
 }
 
 // Para compilar: terminal: javac nome_arquivo.java
